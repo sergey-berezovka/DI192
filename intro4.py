6f9254c01112e4cd1721c0fc6b21410a78730112
Sergey <sergey.berezovka@gmail.com>
2026-02-11 10:26:43 +0200

    test

--- intro4.py
+++ intro4.py
@@ -1,13 +1,15 @@
-# def difference (a,b):
-#     return a - b
-# print(difference(25,5))
+def difference (a,b):
+    return a - b
+print(difference(2,2))
+print(difference(0,2))
 
-x = input()
-def print_day (x):
-    if 8 < x < 1:
-        print("None")
-    else:
-        print_day
+def print_day (num):
+   days = [
+      "Sunday", "Monday", "Tuesday", "Wednеsday", "Thirsday", "Friday", "Saturday"
+      ]
+   if 1 <= num <= 7:
+    return days [num - 1]
+   return None
+print(print_day(4))
+print(print_day(0))
 
-
-print("hi sergei")
